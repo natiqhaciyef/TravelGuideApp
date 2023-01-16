4d0bf635fffcb17cb8f0401f408004fed8f0cd1e
Natiq <natiq00h2272@gmail.com>
2023-01-16 17:26:51 +0400

    Error fixing

--- app/src/main/java/com/natiqhaciyef/travelguideapp/ui/viewmodel/register/RegisterViewModel.kt
+++ app/src/main/java/com/natiqhaciyef/travelguideapp/ui/viewmodel/register/RegisterViewModel.kt
@@ -2,9 +2,10 @@ package com.natiqhaciyef.travelguideapp.ui.viewmodel.register
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
-@AndroidEntryPoint
+@HiltViewModel
 class RegisterViewModel @Inject constructor(
 
 ): ViewModel() {
